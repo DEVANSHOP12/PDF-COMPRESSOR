7251d943e031444c5e25f81b5176036988337f4a
DEVANSHOP12 <101128540+DEVANSHOP12@users.noreply.github.com>
2022-03-10 01:44:17 +0530

    Update markups.py

--- support/markups.py
+++ support/markups.py
@@ -2,14 +2,17 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 start = [
         [
-            InlineKeyboardButton('Support', url='t.me/rmprojects'),
-            InlineKeyboardButton('Source', url='github.com/m4mallu')
+        [
+            InlineKeyboardButton('join US FOR MORE ', url='t.me/TEAM_SILENT_KING'),
+            InlineKeyboardButton('Support', url='t.me/OFF_CHATS'),
+            InlineKeyboardButton('Source', url='github.com/Dvansh20055')
+        ]
         ]
         ]
 
 close = [
         [
-            InlineKeyboardButton('Support', url='t.me/rmprojects'),
+            InlineKeyboardButton('Support', url='t.me/TEAM_SILNET_KING'),
             InlineKeyboardButton('Close', callback_data='close_btn')
         ]
         ]
